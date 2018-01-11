1a0bdef6272b9976136163679af94ed48ff221af
siliyu <sili.yu@unicareer.com>
2018-01-11 20:59:07 +0800

    resolve Q6

---
+++ learn-go/chapter3/Q6.1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	s:=[]float64{1,2,3,4}
+	a:= average(s)
+	fmt.Println(a)
+}
+
+func average(a []float64) (r float64){
+	var total float64
+	for _, num := range(a){
+		total += num
+	}
+	r = total / float64(len(a))
+	return
+}
